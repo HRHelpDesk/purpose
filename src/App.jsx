948f00f77362e6d68d6a9d831269eca109c48fe2
Mason Merrell <masonmerrell@MasonsMcStudio2.lan>
2026-01-26 13:19:15 -0600

    updating website domain setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,28 @@
 // src/App.jsx
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 
 import SquireAuth from './screens/auth/SquireAuth';
 import Booking from './screens/Booking';
 import BarbershopLanding from './screens/LandingPage';
 
-const router = createBrowserRouter(
-  [
-    {
-      path: '/',
-      element: <BarbershopLanding />,
-    },
-    {
-      path: '/square-auth',
-      element: <SquireAuth />,
-    },
-    {
-      path: '/booking',
-      element: <Booking />,
-    },
-    {
-      path: '*',
-      element: <div>404 - Page not found</div>,
-    },
-  ],
+const router = createHashRouter([
   {
-    basename: import.meta.env.BASE_URL,   // ← this is the key line
-  }
-);
+    path: '/',
+    element: <BarbershopLanding />,
+  },
+  {
+    path: '/square-auth',
+    element: <SquireAuth />,
+  },
+  {
+    path: '/booking',
+    element: <Booking />,
+  },
+  {
+    path: '*',
+    element: <BarbershopLanding/>,
+  },
+]);
 
 function App() {
   return <RouterProvider router={router} />;
--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import CssBaseline from '@mui/material/CssBaseline';
 
-import App from './App.jsx'
-import { createTheme, ThemeProvider } from '@mui/material';
+import App from './App.jsx';
+import { ThemeProvider } from '@mui/material';
 import theme from './assets/theme.js';
 
-
-
 createRoot(document.getElementById('root')).render(
-   <ThemeProvider theme={theme}>
-  <StrictMode>
-        <CssBaseline />
-
-    <App />
-  </StrictMode>
-  </ThemeProvider>,
-)
+  <ThemeProvider theme={theme}>
+    <StrictMode>
+      <CssBaseline />
+      <App />
+    </StrictMode>
+  </ThemeProvider>
+);
